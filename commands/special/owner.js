e226bdb2768dcdd4c29eb88e2b7712bc08cfecb8
efef <126972751+tohruegaput@users.noreply.github.com>
2023-03-11 22:16:40 +0700

    Add files via upload

--- commands/special/owner.js
+++ commands/special/owner.js
@@ -7,8 +7,8 @@ exports.run = {
       // if (command == 'owner') return client.reply(m.chat, `Interkasi owner dengan user via WhatsApp sudah tidak tersedia lagi, jika ingin upgrade premium atau ada hal penting lainnya silahkan kirim pesan melalui email dengan subjek *"MINT"*\n\n✉️ : contact@neoxr.my.id`, m)
       if (command == 'owner') {
          const contact = [{
-            name: 'Ragil gay',
-            number: '6285730794089'
+            name: 'Bayu Mascuy',
+            number: '6285882366019'
          }]
          const msg = await client.sendContact(m.chat, contact, m)
          await client.reply(m.chat, `Chat aja gausah sungkan" bang`, msg)
